263ffb1c18d86c65ffd45a1b213a6d8bad6cb40e
Sticki <sticki@qq.com>
2022-09-04 10:25:10 +0800

    feat: init code

---
+++ src/main/java/cn/sticki/test/TestApplication.java
@@ -0,0 +1,16 @@
+package cn.sticki.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 阿杆
+ */
+@SpringBootApplication
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApplication.class, args);
+	}
+
+}
